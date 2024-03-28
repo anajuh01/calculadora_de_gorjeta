ed072e77f2c4fbec14e9a2fe604e9f5c74140fb3
Ana Júlia Viana <anajujubaviana@gmail.com>
2024-03-28 09:03:19 -0300

    nono commit do html e terceiro commit do js

--- scripts.js
+++ scripts.js
@@ -33,9 +33,16 @@ function receberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click")
+    botao.addEventListener("click", receberPorcentagemBotao)
 })
 
-function receberPorcentagem() {
-    porcentagem = Number(evento.target.value) / 100
+function receberPorcentagemBotao(evento) {
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+        if(botao.value === evento.target.value) {
+            botao.classList.add("botao-ativo")
+        }
+
+    })
+    porcentagem = parseFloat(evento.target.value) / 100
 }
