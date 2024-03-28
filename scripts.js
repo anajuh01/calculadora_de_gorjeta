ad657c613f0d072ea9f8ec72e40617d4884e7984
Ana Júlia Viana <anajujubaviana@gmail.com>
2024-03-28 08:25:55 -0300

    setimo commit do html e primeiro commit do js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let conta = 0
+let pessoas = 0
+
+const contaInput = document.querySelector("#conta", receberValorConta)
+contaInput.addEventListener("input")
+function receberValorConta(evento) {
+ 
+    conta= Number(evento.target.velho)
+}
+
+const pessoasInput = document.querySelector("#pessoas")
+pessoasInput.addEventListener("input", receberQuantidadePessoas)
+
+function receberQuantidadePessoas(evento) {
+    const paragrafoErro= document.querySelector(".pessoas #erro")
+    const divErro = document.querySelector(".pessoas .input-box")
+    if(evento.target.value === "0") {
+        
+        console.log(paragrafoErro)
+        paragrafoErro.style.display = "block"
+        divErro.setAttribute("id", "erro-div")
+    
+
+} else {
+        paragrafoErro.style.display = "none"
+        divErro.setAttribute("id", "")
+        pessoas = Number(evento.target.value)
+}
+}
